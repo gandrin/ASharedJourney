529b124e062bd5fa39af1f5adb456e85efee554d
gabriel andrin <gabriela@theodo.fr>
2018-06-30 11:37:32 +0200

    Add listener

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"time"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/gandrin/ASharedJourney/tiles"
 	"golang.org/x/image/colornames"
 )
+
+const frameRate = 60
 
 func run() {
 	cfg := pixelgl.WindowConfig{
@@ -22,8 +26,11 @@ func run() {
 
 	tiles.GenerateMap(win)
 
+	fps := time.Tick(time.Second / frameRate)
+
 	for !win.Closed() {
 		win.Update()
+		<-fps
 	}
 }
 
