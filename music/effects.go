693c88bdc2510a1cc30a1158b5438914d92a6bce
Fabio Maschi <fabio.maschi@u-psud.fr>
2018-07-01 13:51:29 +0200

    music log

--- music/effects.go
+++ music/effects.go
@@ -19,54 +19,36 @@ const (
 
 func (m *musicStreamers) PlayEffect(effectType soundEffect) {
 
-		es, ok := m.gameEffects[effectType]
-		if ok {
-			speaker.Lock()
-			m.streamControl.Paused = true
-			speaker.Unlock()
-<<<<<<< HEAD
-			//log.Print("Creating new stream entry")
-			done := make(chan struct{})
-			//effect exists -> play
-			speaker.Play(beep.Seq(
-				es,
-				beep.Callback(func() {
-					close(done)
-				}),
-			))
-			//log.Print("finished playing effect ")
-			speaker.Lock()
-			m.streamControl.Paused = false
-			speaker.Unlock()
-			<-done
-			//log.Print("stream of sound finished")
-=======
-			log.Print("Creating new stream entry")
-			loopedaudio := beep.Loop(1, es.Streamer(0, es.Len()))
-			speaker.Play(beep.Seq(loopedaudio)) //effect exists -> play
-			log.Print("finished playing effect ")
-			speaker.Lock()
-			m.streamControl.Paused = false
-			speaker.Unlock()
-			log.Print("stream of sound finished")
->>>>>>> d023a5de9cc0263b7555d9143acc752e1ad220fd
-		}
+	es, ok := m.gameEffects[effectType]
+	if ok {
+		speaker.Lock()
+		m.streamControl.Paused = true
+		speaker.Unlock()
+		//log.Print("Creating new stream entry")
+		loopedaudio := beep.Loop(1, es.Streamer(0, es.Len()))
+		speaker.Play(beep.Seq(loopedaudio)) //effect exists -> play
+		//log.Print("finished playing effect ")
+		speaker.Lock()
+		m.streamControl.Paused = false
+		speaker.Unlock()
+		//log.Print("stream of sound finished")
+	}
 
 }
 
 func (m *musicStreamers) loadEffects() {
 	m.gameEffects = make(map[soundEffect]*beep.Buffer, 0)
 	//make new buffers and add to buffer
-	stream1,format1 := getStream(string(SOUND_EFFECT_START_GAME))
-	m.gameEffects[SOUND_EFFECT_START_GAME]= beep.NewBuffer(format1)
+	stream1, format1 := getStream(string(SOUND_EFFECT_START_GAME))
+	m.gameEffects[SOUND_EFFECT_START_GAME] = beep.NewBuffer(format1)
 	m.gameEffects[SOUND_EFFECT_START_GAME].Append(stream1)
 
-	stream2,format2 := getStream(string(SOUND_EFFECT_WIN_GAME))
-	m.gameEffects[SOUND_EFFECT_WIN_GAME]= beep.NewBuffer(format2)
+	stream2, format2 := getStream(string(SOUND_EFFECT_WIN_GAME))
+	m.gameEffects[SOUND_EFFECT_WIN_GAME] = beep.NewBuffer(format2)
 	m.gameEffects[SOUND_EFFECT_WIN_GAME].Append(stream2)
 
-	stream3,format3 := getStream(string(SOUND_EFFECT_WATER))
-	m.gameEffects[SOUND_EFFECT_WATER]= beep.NewBuffer(format3)
+	stream3, format3 := getStream(string(SOUND_EFFECT_WATER))
+	m.gameEffects[SOUND_EFFECT_WATER] = beep.NewBuffer(format3)
 	m.gameEffects[SOUND_EFFECT_WATER].Append(stream3)
 
 }
