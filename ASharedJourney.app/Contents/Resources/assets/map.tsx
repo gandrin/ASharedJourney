c9c68ea3dc0e5866c9e96fc7aa4b406f2f3aef02
gabriel andrin <gabriela@theodo.fr>
2018-07-01 14:39:53 +0200

    Ignore useless stuff

--- ASharedJourney.app/Contents/Resources/assets/map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="map" tilewidth="32" tileheight="32" tilecount="65" columns="5">
- <image source="map.png" width="160" height="416"/>
-</tileset>
