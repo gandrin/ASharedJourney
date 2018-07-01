1e8dec46d03d69fab67cda45b6edb82d0088f530
Pierre Poupin <pierrep@theodo.fr>
2018-07-01 16:26:42 +0200

    Improve mac build

--- build_mac.sh
+++ build_mac.sh
@@ -2,6 +2,6 @@
 
 go build main.go
 chmod +x main
+mkdir -p ./ASharedJourney.app/Contents/MacOS/
 cp main ./ASharedJourney.app/Contents/MacOS/ASharedJourney
 cp -r ./assets ./ASharedJourney.app/Contents/MacOS/
-cp -r ./assets ./ASharedJourney.app/Contents/Resources
