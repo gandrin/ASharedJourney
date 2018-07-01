bb5ef39cc8a7af1ca83dd47c8fa2e4ddbc5db5e1
Pierre Poupin <pierrep@theodo.fr>
2018-07-01 16:34:21 +0200

    Add information in build_mac.sh

--- build_mac.sh
+++ build_mac.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+DESTINATION="./build/ASharedJourney.app"
+
 go build main.go
 chmod +x main
-mkdir -p ./build/ASharedJourney.app/Contents/MacOS/
-cp main ./build/ASharedJourney.app/Contents/MacOS/ASharedJourney
-cp -r ./assets ./build/ASharedJourney.app/Contents/MacOS/
+mkdir -p $DESTINATION/Contents/MacOS/
+cp main $DESTINATION/Contents/MacOS/ASharedJourney
+cp -r ./assets $DESTINATION/Contents/MacOS/
+
+echo "Built!"
+echo "You may now send" $DESTINATION "to your beautiful friends!"
