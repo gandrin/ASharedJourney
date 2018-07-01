8f9461c2c17f07892981d6abc7ed0da7fa07fc1f
Pierre Poupin <pierrep@theodo.fr>
2018-07-01 16:28:53 +0200

    Move .app to build

--- build_mac.sh
+++ build_mac.sh
@@ -2,6 +2,6 @@
 
 go build main.go
 chmod +x main
-mkdir -p ./ASharedJourney.app/Contents/MacOS/
-cp main ./ASharedJourney.app/Contents/MacOS/ASharedJourney
-cp -r ./assets ./ASharedJourney.app/Contents/MacOS/
+mkdir -p ./build/ASharedJourney.app/Contents/MacOS/
+cp main ./build/ASharedJourney.app/Contents/MacOS/ASharedJourney
+cp -r ./assets ./build/ASharedJourney.app/Contents/MacOS/
